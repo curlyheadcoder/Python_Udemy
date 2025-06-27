4ac523f356dc77f28f31cf55120f2251911032fd
mayank <mayanksonwani078@gmail.com>
2025-06-28 02:28:04 +0530

    Added new files for Data Types

---
+++ 03_datatypes/chapter02.py
@@ -0,0 +1,12 @@
+spice_mix = set()
+
+print(f"Initial space mix id: {id(spice_mix)}")
+
+spice_mix.add("Ginger")
+spice_mix.add("Chilli")
+spice_mix.add("Cardamom")
+spice_mix.add("Fennel")
+
+print(f"Initial Spices {spice_mix}")
+# for i in spice_mix:
+#     print(spice_mix[0])
